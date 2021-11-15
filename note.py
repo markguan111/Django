71b64ca1ec14a20a56c22ce9a2460e7cee3cddb6
shuo guan <shuoguan1@baylor.edu>
2021-11-15 17:16:19 -0600

    First

---
+++ note.py
@@ -0,0 +1,14 @@
+'''
+
+get request : get the informaton from database
+post request: send the information to the database
+
+Django is a popular python library that is used to build web apps
+Is a server_side web framework
+
+models: models.py / view.py / URLS.py / templates
+
+pip install -r requirements.txt
+pip freeze > requirements.txt
+
+'''
