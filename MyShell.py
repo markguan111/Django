a3ddd6e277a505e78d9af9652eb301145b962c88
shuo guan <shuoguan1@baylor.edu>
2021-11-17 17:32:00 -0600

    second

---
+++ MyShell.py
@@ -0,0 +1,25 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "learning_log.settings")
+
+import django
+django.setup()
+
+from MainApp.models import Topic, Entry
+
+topics = Topic.objects.all() # select * from Topics
+
+for topic in topics:
+    print(topic.id)
+    print(topic.text)
+    print(topic.date_added)
+
+
+t= Topic.objects.get(id=1)
+print(t)
+
+entries = t.entry_set.all()
+
+for e in entries:
+    print(e)
+    
