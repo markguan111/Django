5d9c959c770f9ff37608a14b8db7f28d087ac946
shuo guan <shuoguan1@baylor.edu>
2021-12-01 17:32:22 -0600

    fourth

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 
 from .models import Topic
 
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -23,3 +24,38 @@ def topic(request,topic_id):
     context = {'topic':topic, 'entries':entries}
 
     return render(request, 'MainApp/topic.html',context)
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+
+    else:
+        form = TopicForm(data=request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('MainApp:topics')
+
+    #dictionary help to pass the data, the data here is the form
+    context = {'form':form} 
+    return render(request,'MainApp/new_topic.html',context)
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm
+
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+
+            return redirect('MainApp:topic', topic_id=topic_id)
+
+
+    context = {'form':form, 'topic':topic} 
+    return render(request,'MainApp/new_entry.html',context)
