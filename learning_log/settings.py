0cfa57555514aaec18f67d95d33dea7681306111
shuo_guan1 <shuo_guan1@baylor.edu>
2021-12-05 19:16:18 -0600

    Five

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     #myapps
     'MainApp',
+    'Users',
+    #third party apps
+    'bootstrap4',   
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
