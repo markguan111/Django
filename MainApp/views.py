f5a10a9c9d414e2264e36ac8551395ab108f9baa
shuo_guan1 <shuo_guan1@baylor.edu>
2021-12-05 19:50:51 -0600

    six

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render ,redirect
 
-from .models import Topic
+from .models import Topic ,Entry
 
 from .forms import TopicForm, EntryForm
 
@@ -59,3 +59,23 @@ def new_entry(request, topic_id):
 
     context = {'form':form, 'topic':topic} 
     return render(request,'MainApp/new_entry.html',context)
+
+def edit_entry(request,entry_id):
+    """Edit an existing entry"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # This argument tells Django to create the form prefilled
+        # with information  from the existing entry object
+        form = EntryForm(instance=entry)
+    else:
+        # POST  data  submiited; process data.
+        form = EntryForm(instance=entry, data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('MainApp:topic',topic_id=topic.id)
+
+    context = {'entry' : entry, 'topic' : topic, 'form' : form}
+    return render(request, 'MainApp/edit_entry.html',context)
+    
